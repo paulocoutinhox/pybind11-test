731d61cda9eebf0c62ac6661c4f567fde5eed596
Paulo Coutinho <paulo@prsolucoes.com>
2020-11-18 17:43:01 -0300

    project update

--- src/python/functions.py
+++ src/python/functions.py
@@ -1,6 +1,5 @@
 import glob
 import os
-import pwd
 import shutil
 import stat
 import sys
@@ -188,11 +187,6 @@ def find_files(directory, pattern):
     ]
 
     return files
-
-
-def is_test_user():
-    user = pwd.getpwuid(os.getuid())[0]
-    return user == "paulo"
 
 
 def file_has_content(file, content):
